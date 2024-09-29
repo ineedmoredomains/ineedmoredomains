2016dba2613386127e7109895bffcac0f209469e
Jersey <wgyt735yt@gmail.com>
2024-09-29 14:48:12 -0400

    redo the site

--- README.md
+++
@@ -1,8 +0,0 @@
-# ineedmore.domains website
-## Developing this
-### yarn dev
-Start a dev server
-### yarn build / yarn generate
-Build the site
-### yarn preview
-Preview the built site
--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['./index.html', './pages/**/*.{vue,js,ts,jsx,tsx,scss}', './src/**/*.{vue,js,ts,jsx,tsx,scss}'],
-  theme: {
-    fontFamily: {
-      openly: ['Openly'],
-    },
-  },
-  plugins: [],
-};
